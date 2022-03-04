7a4497b45edf6cf7d97ded53f3ed5a2cbf6f0014
Lorenze Jay Hernandez <63378463+undefined@users.noreply.github.com>
2022-03-04 19:51:15 +0000

    First commit

---
+++ src/index.svelte
@@ -0,0 +1,36 @@
+<svelte:options tag="my-counter" />
+
+<script lang="ts">
+	let status = 'start';
+	let count = 0;
+
+	function startTimer(){
+		setInterval(() => {
+		status = 'stop';
+		count = count+1;
+		},1000)
+	}
+
+	function dec() {
+		count--;
+	}
+</script>
+
+<style>
+	* {
+		font-size: 200%;
+	}
+
+	.input-btn {
+		display: flex;
+	}
+</style>
+
+<div>
+
+	<div class="input-btn">
+		<input />
+		<button on:click={startTimer}>{status}</button>
+	</div>
+	<div>{count}</div>
+</div>
